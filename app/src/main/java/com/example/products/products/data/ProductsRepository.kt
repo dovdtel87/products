5aad04ea8e36b58ad3ecc1cf3cdf27be873e3bd0
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-14 18:57:23 +0200

    Creates the ProductRepository and its implementation

---
+++ app/src/main/java/com/example/products/products/data/ProductsRepository.kt
@@ -0,0 +1,4 @@
+package com.example.products.products.data
+
+interface ProductsRepository {
+}
---
+++ app/src/main/java/com/example/products/products/data/ProductsRepositoryImpl.kt
@@ -0,0 +1,7 @@
+package com.example.products.products.data
+
+import javax.inject.Inject
+
+class ProductsRepositoryImpl @Inject constructor(): ProductsRepository {
+
+}
