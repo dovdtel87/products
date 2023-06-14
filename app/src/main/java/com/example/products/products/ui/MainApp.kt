ce4cfdab3b6f5041538a432c84e3ded942fe839c
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-14 18:34:46 +0200

    Adds libraries for navigation

---
+++ app/src/main/java/com/example/products/products/ui/MainApp.kt
@@ -0,0 +1,29 @@
+package com.example.products.products.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
+import com.example.products.products.ui.checkout.CheckoutScreen
+import com.example.products.products.ui.list.ListProductsScreen
+import com.example.products.ui.theme.ProductsTheme
+
+@Composable
+fun MainApp() {
+    val navController = rememberNavController()
+    ProductsTheme {
+        NavHost(navController = navController, startDestination = "list") {
+            composable("list") {
+                ListProductsScreen() {
+                    navController.navigate("checkout")
+                }
+            }
+            composable(
+                route = "checkout"
+            ) {
+                CheckoutScreen()
+            }
+        }
+    }
+}
