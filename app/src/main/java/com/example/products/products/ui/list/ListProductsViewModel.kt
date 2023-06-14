e84521a1212bc3268b304728d9373d787a82412f
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-14 18:58:07 +0200

    Creates the ViewModel

---
+++ app/src/main/java/com/example/products/products/ui/list/ListProductsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.products.products.ui.list
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListProductsViewModel @Inject constructor(): ViewModel() {
+
+}
