6f4ef6608872f3e79498ee9602aa390bce62cd10
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-14 18:57:02 +0200

    Creates the ProductsApi class

---
+++ app/src/main/java/com/example/products/products/data/ProductsApi.kt
@@ -0,0 +1,6 @@
+package com.example.products.products.data
+
+interface ProductsApi {
+
+    //URL https://gist.githubusercontent.com/palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json
+}
