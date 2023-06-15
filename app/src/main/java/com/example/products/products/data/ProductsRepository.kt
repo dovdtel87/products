19da33a7a47ebc9fd912ec51ad216902114e3776
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-15 10:50:07 +0200

    Creates methods to retrieve products in the repository

--- app/src/main/java/com/example/products/products/data/ProductsRepository.kt
+++ app/src/main/java/com/example/products/products/data/ProductsRepository.kt
@@ -1,4 +1,7 @@
 package com.example.products.products.data
 
+import com.example.products.products.data.model.Product
+
 interface ProductsRepository {
+    suspend fun fetchProducts() :  Result<List<Product>>
 }
--- app/src/main/java/com/example/products/products/data/ProductsRepositoryImpl.kt
+++ app/src/main/java/com/example/products/products/data/ProductsRepositoryImpl.kt
@@ -1,7 +1,18 @@
 package com.example.products.products.data
 
+import com.example.products.products.data.mapper.ProductMapper
+import com.example.products.products.data.model.Product
 import javax.inject.Inject
 
-class ProductsRepositoryImpl @Inject constructor(): ProductsRepository {
-
+class ProductsRepositoryImpl @Inject constructor(
+    private val productsApi: ProductsApi,
+    private val mapper : ProductMapper
+): ProductsRepository {
+    override suspend fun fetchProducts(): Result<List<Product>> {
+        return try {
+            Result.success(mapper.map(productsApi.fetchProducts().products))
+        } catch (exception : Exception) {
+            Result.failure(exception)
+        }
+    }
 }
