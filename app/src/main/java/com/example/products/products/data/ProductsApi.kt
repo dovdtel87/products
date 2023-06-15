0f2f64ab756fa716f4b641f4ffda1de5e5c3880d
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-15 10:49:15 +0200

    Creates api method

--- app/src/main/java/com/example/products/products/data/ProductsApi.kt
+++ app/src/main/java/com/example/products/products/data/ProductsApi.kt
@@ -1,6 +1,12 @@
 package com.example.products.products.data
 
+import com.example.products.products.data.network.model.ProductResponseDto
+import retrofit2.http.GET
+
 interface ProductsApi {
 
     //URL https://gist.githubusercontent.com/palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json
+
+    @GET("Products.json")
+    suspend fun fetchProducts() : ProductResponseDto
 }
