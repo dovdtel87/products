7099e10b2b62718ea4f53d5f84a951de583e3e9e
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-15 18:08:10 +0200

    Formats the message with the price correctly

--- app/src/main/java/com/example/products/products/data/model/Discount.kt
+++ app/src/main/java/com/example/products/products/data/model/Discount.kt
@@ -1,11 +1,13 @@
 package com.example.products.products.data.model
 
+import com.example.products.products.data.extensions.formatPriceAsEuro
+
 sealed class Discount(
     val numberToBuy: Int,
     val numberFree: Int = 0,
     val directDiscount: Double = 0.0,
     val message: String
 ) {
-    class FreeItem(numberToBuy: Int, numberFree: Int, message: String = "Buy $numberToBuy and get {$numberFree} for free!") : Discount(numberToBuy, numberFree, message = message)
-    class PriceReduction(numberToBuy: Int, directDiscount: Double, message: String = "Buy $numberToBuy and get {$directDiscount} in each"): Discount(numberToBuy, directDiscount = directDiscount, message = message)
+    class FreeItem(numberToBuy: Int, numberFree: Int, message: String = "Buy $numberToBuy and get $numberFree for free!") : Discount(numberToBuy, numberFree, message = message)
+    class PriceReduction(numberToBuy: Int, directDiscount: Double, message: String = "Buy $numberToBuy and get ${directDiscount.formatPriceAsEuro()} discount in each"): Discount(numberToBuy, directDiscount = directDiscount, message = message)
 }
