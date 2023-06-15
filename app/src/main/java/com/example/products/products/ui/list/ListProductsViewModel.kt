80a5a2c2d84441becc7a06c323b4c46c6caf7e83
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-15 10:50:44 +0200

    Fetch the products from the viewmodel

--- app/src/main/java/com/example/products/products/ui/list/ListProductsViewModel.kt
+++ app/src/main/java/com/example/products/products/ui/list/ListProductsViewModel.kt
@@ -1,10 +1,34 @@
 package com.example.products.products.ui.list
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.products.products.data.ProductsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ListProductsViewModel @Inject constructor(): ViewModel() {
+class ListProductsViewModel @Inject constructor(
+    private val productsRepository: ProductsRepository
+): ViewModel() {
+    
+    init {
+        fetchProducts()
+    }
 
+    fun fetchProducts() {
+        Log.d("Test", "Fetching products")
+
+        viewModelScope.launch {
+            productsRepository.fetchProducts()
+                .onSuccess { products ->
+                    products.forEach {
+                        Log.d("Test", it.name)
+                    }
+                }.onFailure {
+                    Log.d("Test", "There where an error")
+                }
+        }
+    }
 }
