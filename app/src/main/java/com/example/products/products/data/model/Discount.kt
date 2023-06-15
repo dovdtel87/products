e53330f47f8dae924644b5c5945b89e5796bd8ce
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-15 17:17:41 +0200

    Updates discounts to support message

--- app/src/main/java/com/example/products/products/data/model/Discount.kt
+++ app/src/main/java/com/example/products/products/data/model/Discount.kt
@@ -3,8 +3,9 @@ package com.example.products.products.data.model
 sealed class Discount(
     val numberToBuy: Int,
     val numberFree: Int = 0,
-    val directDiscount: Double = 0.0
+    val directDiscount: Double = 0.0,
+    val message: String
 ) {
-    class FreeItem(numberToBuy: Int, numberFree: Int) : Discount(numberToBuy, numberFree)
-    class PriceReduction(numberToBuy: Int, directDiscount: Double): Discount(numberToBuy, directDiscount = directDiscount)
+    class FreeItem(numberToBuy: Int, numberFree: Int, message: String = "Buy $numberToBuy and get {$numberFree} for free!") : Discount(numberToBuy, numberFree, message = message)
+    class PriceReduction(numberToBuy: Int, directDiscount: Double, message: String = "Buy $numberToBuy and get {$directDiscount} in each"): Discount(numberToBuy, directDiscount = directDiscount, message = message)
 }
