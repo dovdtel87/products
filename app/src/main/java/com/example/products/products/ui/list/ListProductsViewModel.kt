ae2a306d80bf1dfbd028af64f2e48c163d13c897
dovdtel87 <dmgdavid2109@gmail.com>
2023-06-15 10:55:30 +0200

    Updates the state when the products are received

--- app/src/main/java/com/example/products/products/ui/list/ListProductsViewModel.kt
+++ app/src/main/java/com/example/products/products/ui/list/ListProductsViewModel.kt
@@ -4,7 +4,11 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.products.products.data.ProductsRepository
+import com.example.products.products.ui.list.state.ListScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,7 +16,10 @@ import javax.inject.Inject
 class ListProductsViewModel @Inject constructor(
     private val productsRepository: ProductsRepository
 ): ViewModel() {
-    
+
+    private val _state = MutableStateFlow(ListScreenState())
+    val state = _state.asStateFlow()
+
     init {
         fetchProducts()
     }
@@ -25,6 +32,12 @@ class ListProductsViewModel @Inject constructor(
                 .onSuccess { products ->
                     products.forEach {
                         Log.d("Test", it.name)
+                    }
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            products = products
+                        )
                     }
                 }.onFailure {
                     Log.d("Test", "There where an error")
